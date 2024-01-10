98c2c7c385f42d4a5ef9515210e0fc1692fff54f
Jim Roger Herrera <109255019+jhengineering@users.noreply.github.com>
2024-01-10 12:07:58 -0400

    Update lang.js

--- src/lang.js
+++ src/lang.js
@@ -4,10 +4,10 @@ function changeLanguage() {
     var language = document.getElementById("language").value;
     if (language == "en") {
       // Change to English
-        window.location.href = "index.html";
+        window.location.href = "/index.html";
     }else if (language == "es") {
       // Change to Español
-        window.location.href = "public/es/esindex.html";
+        window.location.href = "public/es/index.html";
     }else if (language == "ge") {
         // Change to Germany
         window.location.href = "public/ge/index.html";
